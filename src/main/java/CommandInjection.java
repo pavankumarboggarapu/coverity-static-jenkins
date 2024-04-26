131fdcf68e99cf348a5bd25c2e536ea6777ff00e
chuckaude <21095583+chuckaude@users.noreply.github.com>
2024-04-26 16:39:44 -0700

    add CommandInjection.java

---
+++ src/main/java/CommandInjection.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import javax.servlet.http.HttpServletRequest;
+
+public class CommandInjection {
+    public static Process runCmd(HttpServletRequest request) throws IOException {
+        String filename = request.getParameter("filename");
+        //String filename = System.console().readLine();
+        ProcessBuilder builder = new ProcessBuilder("cat",filename);
+        Process process = builder.start();
+        return(process);
+    }
+}
