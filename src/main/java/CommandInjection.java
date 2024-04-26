37f2776b0e62e2748ec8e249b208070178f31007
chuckaude <21095583+chuckaude@users.noreply.github.com>
2024-04-26 16:05:36 -0700

    Update CommandInjection.java

--- src/main/java/CommandInjection.java
+++ src/main/java/CommandInjection.java
@@ -3,8 +3,8 @@ import javax.servlet.http.HttpServletRequest;
 
 public class CommandInjection {
     public static Process runCmd(HttpServletRequest request) throws IOException {
-        //String filename = request.getParameter("filename");
-        String filename = System.console().readLine();
+        String filename = request.getParameter("filename");
+        //String filename = System.console().readLine();
         String[] cmdList = new String[]{"cat" + filename};
         ProcessBuilder builder = new ProcessBuilder(cmdList);
         Process process = builder.start();
